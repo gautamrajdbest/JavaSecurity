76179898dcf22eb407fd825117408fece55ba973
gautamrajdbest <me2xps@gmail.com>
2017-11-23 11:37:01 +0530

    Generic Class
    Testing the Generic Class Test example

---
+++ GenericClassTest.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class GenericClassTest {
+
+	public static void main(String[] args) {
+		Integer[] intArray = {90,70,60,42,51,10,20,30,40,80};
+		Character[] charArray = {'Z','X','A','D','V','M','1','0','B','E','C','F'};
+		
+		System.out.println("Printing Integer Array [] : ");
+		printArray(intArray);
+		
+		System.out.println("Printing Character Array [] : ");
+		printArray(charArray);
+	}
+	
+	public static <A> void printArray(A[] elements){
+		for(A element : elements){
+			System.out.println(element);
+		}
+		Arrays.sort(elements);
+		System.out.println("Your Array : "+Arrays.toString(elements));
+	}
+}
